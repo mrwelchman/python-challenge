c0a250baf2340dd1f26870ae9d1e0e7fd52dffdb
Michael Welch <michaelwelch@Michaels-MacBook-Pro.local>
2021-01-09 23:51:33 -0700

    completed homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,100 @@
+# create python script that finds
+    # total months
+    # net total amount of profit/losses
+    # changes in profit/loses then avg
+    # greatest increase in profts
+    # greatest decrease in losses
+    # print to terminal
+    # export to text file
+    
+# dependencies
+import os
+import csv
 
+# set file path
+budgetdatacsvpath = os.path.join('Resources', 'budget_data.csv')
+
+# lists
+months = []
+changes = []
+dates = []
+
+# set initials
+count = 0
+total = 0
+current = 0
+previous = 0
+change = 0
+
+# read the csv
+with open(budgetdatacsvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    
+    # header
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        # count months
+        count += 1
+        
+        # collect months into list
+        months.append(row[0])
+        
+        # calculate total amount
+        current = int(row[1])
+        total += current
+        
+        # taking care of the first change value
+        if (count == 1):
+            previous = current
+            continue
+            
+        else:
+        # find change between months
+            change = current - previous
+            # collect changes into list
+            changes.append(change)
+            # collect dates into list
+            dates.append(row[0])
+            # set current to previous
+            previous = current
+        
+    # sum and average of changes
+    sum_changes = sum(changes)
+    avg_change = round(sum_changes/(count - 1),2)
+        
+    # find max and min change
+    greatest_increase = max(changes)
+    greatest_decrease = min(changes)
+        
+    # find index value that corresponds to the max and min
+    best_change = changes.index(greatest_increase)
+    worst_change = changes.index(greatest_decrease)
+    
+    # find the corresponding dates
+    best_month = dates[best_change]
+    worst_month = dates[worst_change]
+        
+# print to terminal
+print("")
+print("Financial Analysis")
+print("-------------------------")
+print(f"Total Months: {count}")
+print(f"Total: ${total}")
+print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {best_month} (${greatest_increase})")
+print(f"Greatest Decrease in Loses: {worst_month} (${greatest_decrease})")
+print("")
+
+# export to text file
+analysis = os.path.join('analysis', 'analysis_file.txt')
+with open(analysis, "w") as analysis_file:
+    analysis_file.write("\n")
+    analysis_file.write("Financial Analysis\n")
+    analysis_file.write("---------------------------\n")
+    analysis_file.write(f"Total Months: {count}\n")
+    analysis_file.write(f"Total: ${total}\n")
+    analysis_file.write(f"Average Change: ${avg_change}\n")
+    analysis_file.write(f"Greatest Increase in Profits: {best_month} (${greatest_increase})\n")
+    analysis_file.write(f"Greatest Decrease in Loses: {worst_month} (${greatest_decrease})\n")
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,86 @@
+# create a python script that finds
+    # total votes
+    # list of candidates with
+        # % of votes
+        # number of votes
+    # winner
+    
+# dependencies
+import os
+import csv
 
+# set file path
+electiondatacsvpath = os.path.join('Resources','Resources','election_data.csv')
+
+# create dictionary containing candidate and votes received
+candivotes = {}
+
+total_votes = 0
+
+# read the csv
+with open(electiondatacsvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    # header
+    csv_header = next(csvreader)
+    
+    for row in csvreader:
+        # adds unique candidates to dictionary and counts their votes as well as a total vote count
+        total_votes += 1
+        if row[2] in candivotes.keys():
+            candivotes[row[2]] = candivotes[row[2]] + 1
+        else:
+            candivotes[row[2]] = 1
+       
+# create lists
+candidates =[]
+votes = []
+
+# add dictionary values to list
+for key, value in candivotes.items():
+    candidates.append(key)
+    votes.append(value)
+
+# add percent list
+vote_pct = []
+for n in votes:
+    vote_pct.append(round(n/total_votes*100, 1))
+
+# convert to tuples
+clean_data = list(zip(candidates, votes, vote_pct))
+
+# winner
+winner_list = []
+
+for name in clean_data:
+    if max(votes) == name[1]:
+        winner_list.append(name[0])
+
+winner = winner_list[0]
+    
+# print to terminal
+print("")
+print("Election Results")
+print("----------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------")
+for entry in clean_data:
+    print(entry[0] + ": " + str(entry[2]) +"% (" + str(entry[1]) +")")
+print("----------------------")
+print(f"Winner: {winner}")
+print("----------------------")
+print("")
+
+# print to text file
+analysis = os.path.join('analysis', 'analysis_file.txt')
+with open(analysis, 'w') as analysis_file:
+    analysis_file.write("\n")
+    analysis_file.write("Election Results\n")
+    analysis_file.write("----------------------\n")
+    analysis_file.write(f"Total Votes: {total_votes}")
+    analysis_file.write("----------------------")
+    for entry in clean_data:
+        analysis_file.writelines(entry[0] + ": " + str(entry[2]) +"% (" + str(entry[1]) +")\n")
+    analysis_file.write("----------------------\n")
+    analysis_file.write(f"Winner: {winner}\n")
+    analysis_file.write("----------------------\n")
+    analysis_file.write("\n")
